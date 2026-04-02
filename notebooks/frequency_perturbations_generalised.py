4648cae66852333a045f3c78ed42519764819702
Eliot Finch <eliotfinch@gmail.com>
2026-04-01 17:32:09 -0700

    Compute epsilon grids for all start times

--- notebooks/frequency_perturbations_generalised.py
+++ notebooks/frequency_perturbations_generalised.py
@@ -12,38 +12,71 @@ sim = sim_info['sim']
 chif = sim.chif_mag
 Mf = sim.Mf
 
+N_max = 15
+mode_list = utils.get_mode_list(N_max)
+
 t0_E = pd.read_csv('../data/t0N_E.csv', index_col=0)
-t0 = t0_E[str(ID)].values[10]
 
-N = 13
-modes = [(2, 2, n, 1) for n in range(N+1)] + [(3, 2, 0, 1)]
+for i, t0 in enumerate(t0_E[str(ID)].values[:11]):
 
-delta_list_dense = np.linspace(-0.2, 0.2, 100)
+    if i == 0:
+        continue
 
-for ntilde in range(N+1):
+    print(f"t0_{i} = {t0}", flush=True)
 
-    print(f'Calculating epsilon grid for ntilde={ntilde}...')
+    epsilon = 1
 
-    epsilon_grid = np.zeros((len(delta_list_dense), len(delta_list_dense)))
+    for j, modes in enumerate(mode_list):
 
-    for i, delta_i in enumerate(delta_list_dense[::-1]):
-        for j, delta_r in enumerate(delta_list_dense):
-            frequency_deltas_r = np.zeros(len(modes))
-            frequency_deltas_i = np.zeros(len(modes))
-            frequency_deltas_r[ntilde] = delta_r
-            frequency_deltas_i[ntilde] = delta_i
-            epsilon, Mf_bestfit, chif_bestfit = qnmfits.calculate_epsilon(
-                times=sim.times,
-                data=sim.h[2, 2],
-                modes=modes,
-                Mf=sim.Mf,
-                chif=sim.chif_mag,
-                t0=t0,
-                t0_method='closest',
-                T=100,
-                delta_r=frequency_deltas_r,
-                delta_i=frequency_deltas_i
-            )
-            epsilon_grid[i, j] = epsilon
+        new_epsilon = qnmfits.calculate_epsilon(
+            sim.times,
+            sim.h[2, 2],
+            modes + [(3, 2, 0, 1)],
+            Mf,
+            chif,
+            t0=t0,
+            t0_method='closest',
+        )[0]
 
-    np.save(f'epsilon_grid_n{ntilde}.npy', epsilon_grid)
+        if new_epsilon < epsilon:
+            epsilon = new_epsilon
+        else:
+            break
+
+    N = j - 1
+    print(f"N = {N}, epsilon = {epsilon}", flush=True)
+
+    modes = [(2, 2, n, 1) for n in range(N+1)] + [(3, 2, 0, 1)]
+
+    delta_list_dense = np.linspace(-0.2, 0.2, 100)
+
+    for ntilde in range(N+1):
+
+        print(f'Calculating epsilon grid for ntilde={ntilde}...', flush=True)
+
+        epsilon_grid = np.zeros((len(delta_list_dense), len(delta_list_dense)))
+
+        for i, delta_i in enumerate(delta_list_dense[::-1]):
+            for j, delta_r in enumerate(delta_list_dense):
+                frequency_deltas_r = np.zeros(len(modes))
+                frequency_deltas_i = np.zeros(len(modes))
+                frequency_deltas_r[ntilde] = delta_r
+                frequency_deltas_i[ntilde] = delta_i
+                epsilon, Mf_bestfit, chif_bestfit = qnmfits.calculate_epsilon(
+                    times=sim.times,
+                    data=sim.h[2, 2],
+                    modes=modes,
+                    Mf=sim.Mf,
+                    chif=sim.chif_mag,
+                    t0=t0,
+                    t0_method='closest',
+                    T=100,
+                    delta_r=frequency_deltas_r,
+                    delta_i=frequency_deltas_i
+                )
+                epsilon_grid[i, j] = epsilon
+
+        np.save(
+            f'../data/epsilon_grids/t0_{i}_N{N}_ntilde{ntilde}.npy',
+            epsilon_grid
+        )
